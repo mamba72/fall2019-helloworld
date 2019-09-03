7a31393e13c4ab706cd21ad0174a4bef7d9bc3bb
Stephen White <saltybanana72@gmail.com>
2019-09-03 15:28:16 -0700

    More comments

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,8 @@ using namespace std;
 //main function
 int main(int argc, char** argv)
 {
+	//print to the screen
 	cout << "Hello World!\n";
+	//return 0 to signify that the program ended successfully
 	return 0;
 }
